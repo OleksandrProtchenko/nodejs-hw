f69526ee93ca724c40cf6997351b09a1e22b37fc
Oleksandr Protchenko <alexdevelop92@gmail.com>
2026-05-22 18:33:17 +0300

    add 02-express hw

---
+++ src/models/note.js
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    content: { type: String, required: false, trim: true, default: '' },
+    tag: {
+      type: String,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+      required: false,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Note = model('Note', noteSchema);
