8e058e2de376c5ee0543bd82b870f85e48ebb401
Oleksandr Protchenko <alexdevelop92@gmail.com>
2026-05-31 03:07:27 +0300

    add 05-mail-and-img hw on node

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import dns from 'node:dns/promises';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import cookieParser from 'cookie-parser';
 
 dns.setServers(['8.8.8.8']);
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 
